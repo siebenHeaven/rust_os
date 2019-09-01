191b8cc80935af825dfcf5742ca6767a7afece5d
Anup Mahindre <anup.mahindre.7@gmail.com>
2019-09-01 20:06:50 +0530

    Add page translation function
    Signed-off-by: Anup Mahindre <anup.mahindre.7@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
@@ -36,8 +37,13 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(rust_os::test_runner)]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::{print, println};
 
@@ -21,19 +22,32 @@ fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info);
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory::translate_addr;
+    use x86_64::VirtAddr;
+
     println!("Hello world{}", "!");
 
     rust_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x20010a,
+        // some stack page
+        0x57ac_001f_fe48,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, boot_info.physical_memory_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
